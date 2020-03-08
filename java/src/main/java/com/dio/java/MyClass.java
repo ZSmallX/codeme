0ddce958dafba681da565a906fe287a59efbda1b
Dio <389182685@qq.com>
2020-03-08 16:54:05 +0800

    init with java module only.

---
+++ java/src/main/java/com/dio/java/MyClass.java
@@ -0,0 +1,12 @@
+package com.dio.java;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyClass {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
